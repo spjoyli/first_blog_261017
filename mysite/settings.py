b96a4c85604ba1a6072e93544429c07658ca3c7f
Spjoyli <spjoyli@gmail.com>
2017-10-26 16:57:43 +0100

    fixing_host_name

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9e@5m#qu94jz*%1(=c)tvuep&@c_!9ntr0hgm!41x1a^0q*d5@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'spjoyligx.pythonanywhere.com']
 
 
 # Application definition
